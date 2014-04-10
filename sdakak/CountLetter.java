5f25508721a881f317d31172d48be7acdfb5f1f9
Sdakak Zui <sdakak.zui@gmail.com>
2014-04-09 20:07:25 -0400

    adding sdakak's code

---
+++ sdakak/CountLetter.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.nio.file.*;
+
+public class CountLetter {
+	public static void main(String[] args) throws IOException 
+	{
+			char target = args[0].charAt(0);
+			Path file = Paths.get("xanadu.txt");
+			
+			int count = 0;
+			
+			try (InputStream inputStream = Files.newInputStream(file);
+					BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream)))
+					{
+					String currentLine = bufferedReader.readLine();
+					while (currentLine != null) 
+					{
+							for (int i = 0; i < currentLine.length(); i++) {
+									if (target == currentLine.charAt(i)) {
+											count++;
+									}
+							}
+						currentLine = bufferedReader.readLine();
+					}
+			} 
+			
+			System.out.print(count + " instances of '" + target + "' found in " + file);
+	}
+}
